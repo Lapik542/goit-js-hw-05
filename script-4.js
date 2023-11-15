cd4d74b0a81a57c15dea89044fa567cd68aa9662
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-15 20:53:31 +0200

    Update script-4.js

--- script-4.js
+++ script-4.js
@@ -1,5 +1,4 @@
 const getTotalBalanceByGender = (users, gender) => {
-   // return users.filter(user => user.gender === gender).reduce((acc, user) => acc + user.balance);
    return users
    .filter(user => user.gender === gender)
    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
