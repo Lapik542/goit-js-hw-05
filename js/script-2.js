48dd1e291fe2aa3b515e71dcc350188ea533a9d3
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-16 19:48:58 +0200

    main

--- js/script-2.js
+++ js/script-2.js
@@ -1,6 +1,4 @@
-const getUsersWithFriend = (users, friendName) => {
-   return users.filter(user => user.friends.includes(friendName));
-};
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
 
 const allUser = [
